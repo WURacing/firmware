474512596a248fa1264cc934bd8b33aa185a0c39
Connor Monahan <cma2714@gmail.com>
2021-03-07 13:40:06 -0600

    Cleanup

--- 2020/Controllers/Datalogger/Applications/app_main.c
+++ 2020/Controllers/Datalogger/Applications/app_main.c
@@ -12,8 +12,6 @@
 static FATFS fs;
 static FIL fp;
 
-static uint8_t write_data_perf[4096];
-
 void app_init(void)
 {
 	// enable pull-ups on disconnected pins
@@ -23,11 +21,6 @@ void app_init(void)
 		PORT_REGS->GROUP[0].PORT_PINCFG[paUnused[i]] = PORT_PINCFG_PULLEN(1);
 	for (unsigned int i = 0; i < sizeof (pbUnused) / sizeof (pbUnused[0]); ++i)
 		PORT_REGS->GROUP[1].PORT_PINCFG[pbUnused[i]] = PORT_PINCFG_PULLEN(1);
-	
-	for (int i = 0; i < 4096; ++i)
-	{
-		write_data_perf[i] = 3*i;
-	}
 	
 	f_mount(&fs, "", 0);
 }
@@ -54,38 +47,6 @@ void app_periodic(void)
 	else
 	{
 		puts("Failed to open file for reading");
-	}
-	UINT bw;
-	result = f_open(&fp, "perf.txt", FA_CREATE_ALWAYS | FA_WRITE);
-	if (result == FR_OK)
-	{
-		puts("Opened file for writing");
-#if 0
-		puts("SD CARD WRITE PERFORMANCE TEST START"); // wonder what happens if we get preempted
-		for (int i = 0; i < 1024; ++i)
-		{
-			// write 4MB
-			FRESULT res = f_write(&fp, write_data_perf, 4096, &bw);
-			if (res)
-			{
-				printf("\r\nDISK WRITE ERROR %d\r\n", res);
-				break;
-			}
-			if (bw < 4096)
-			{
-				printf("\r\nDISK FULL ERROR %d\r\n", bw);
-				break;
-			}
-			if ((i % 64) == 0) printf("%d\r\n", i);
-		}
-		puts("DONE WRITING 4MB");
-		///f_puts("Hey guys from uC!", &fp);A
-#endif
-		f_close(&fp);
-	}
-	else
-	{
-		puts("Failed to open file for writing");
 	}
 }
 
