c9023ddf1fa084900ee456dd67d19efcbc0bbb27
donnellca <donnellca@wustl.edu>
2021-12-05 22:34:17 -0600

    updated w/ ads79 module changes

--- CANPico test code/main.py
+++ CANPico test code/main.py
@@ -2,15 +2,23 @@ from ads79_SPI import ads79
 from rp2 import CAN, CANFrame, CANID
 from utime import sleep, sleep_ms
 
-adc = ads79.default_init()
+adc = ads79()
 
-adc.manual_cycle(cycle=[2,3,4])
-adc.prog()
+adc.manual_select(cycle=[2,3,4]) # walks through channel 2, 3, then 4
+adc.prog() # sends programming sequence to ads79, must be done before receiving data
 
 c = CAN()
 ch=0
 
+# adc.run() doesn't do CAN yet, so refactored here
 while True:
-    x = adc.run().to_bytes(8,'little')
-    print(bin(x))
+    frame_data = 0
+    for sdi in adc.run_sequence:
+        adc.send_recv(sdi) # sends channel select frame and recieves previous channel reading in adc.input
+        ch,data = adc.input.ADC_val()
+        frame_data = (frame_data << (12)) + data
+        print("Ch: " + ch + ", data: " + data)
+        sleep_ms(250)
+    x = frame_data.to_bytes(8,'little')
+    print(x)
     c.send_frame(CANFrame(CANID(2),data=x))
