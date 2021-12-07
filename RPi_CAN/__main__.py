63613f2feac3c2ae4099eda636e7d8ffc49c81d4
ShaneWarga <54457686+ShaneWarga@users.noreply.github.com>
2021-12-06 22:19:15 -0600

    locked call

--- RPi_CAN/__main__.py
+++ RPi_CAN/__main__.py
@@ -68,9 +68,10 @@ class LTE_Listener():
             print("locked, passing")
             return
         self.lock.acquire()
-        t.sleep(0.5)
+        #t.sleep(0.5)
         message = {"timestamp":message.timestamp,"arbitration_id":message.arbitration_id,"dlc": message.dlc,"data":int.from_bytes(message.data, "little") ,"dbc_target": self.target}
         self.mqtt_connection.publish(topic=self.TOPIC, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
+        t.sleep(0.3)
         self.lock.release()
 
 if __name__ == "__main__":
