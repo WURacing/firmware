2f469f1a49981c4983cc137373e95da64ac3032e
Hayden Schroeder <hschroeder0317@gmail.com>
2022-03-06 22:14:43 -0600

    struct.unpack() example

---
+++ RPi Pico/Paddle Shifting/StructExample.py
@@ -0,0 +1,6 @@
+from struct import unpack
+import struct
+# >HHHH specifies our format, in this case we specify reading the data as four unsigned shorts in big endian
+# b'\xbe\x05\xbe\x05\x00\x00\x00\xff specifies our data 'be05be05000000ff' given in byte format
+data = struct.unpack('>HHHH', bytes(b'\xbe\x05\xbe\x05\x00\x00\x00\xff'))
+print(data)
