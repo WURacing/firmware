b86626be871f52695ca435aca75f10bc914801a7
Hayden Schroeder <hschroeder0317@gmail.com>
2023-01-28 16:04:17 -0600

    Working average code :hourglass_flowing_sand:

--- Rolling_Average_accel/Rolling_Average_accel.ino
+++ Rolling_Average_accel/Rolling_Average_accel.ino
@@ -11,8 +11,9 @@ byte x_send;
 byte y_send;
 byte z_send;
 
-int datacount=0;
-const int rollingAverage=64;
+int datacount = 0;
+
+const int rollingAverage = 64;
 
 byte x_avgs[rollingAverage];
 byte y_avgs[rollingAverage];
@@ -30,19 +31,18 @@ unsigned long currentMillis = millis();
 unsigned long previousMillis = 0;
 const long interval = 1000;
 
-void blink(){
+void blink() {
   currentMillis = millis();
-  if (currentMillis - previousMillis >= interval){
+  if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    if (ledState == LOW){
+    if (ledState == LOW) {
       ledState = HIGH;
       //Serial.printf("Led High!\n");
-    }
-    else{
+    } else {
       ledState = LOW;
       //Serial.printf("Led Low!\n");
     }
-    digitalWrite(LED,ledState);
+    digitalWrite(LED, ledState);
   }
 }
 
@@ -54,30 +54,31 @@ void blink(){
 
 void setup() {
 
-  
 
-  pinMode(LED,OUTPUT);
+
+  pinMode(LED, OUTPUT);
 
   Serial.begin(11520);
-  pinMode(CS,OUTPUT);
-  digitalWrite(CS,HIGH);
+  pinMode(CS, OUTPUT);
+  digitalWrite(CS, HIGH);
 
   SPI.begin();
-  SPI.beginTransaction(SPISettings(10000000,MSBFIRST,SPI_MODE3));
+  SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE3));
 
-  digitalWrite(CS,LOW);
+  digitalWrite(CS, LOW);
   SPI.transfer(0x20);
   SPI.transfer(0x3F);
-  digitalWrite(CS,HIGH);
+  digitalWrite(CS, HIGH);
 
-  pinMode(PIN_CAN_STANDBY,OUTPUT);
-  digitalWrite(PIN_CAN_STANDBY,false);
-  pinMode(PIN_CAN_BOOSTEN,OUTPUT);
-  digitalWrite(PIN_CAN_BOOSTEN,true);
+  pinMode(PIN_CAN_STANDBY, OUTPUT);
+  digitalWrite(PIN_CAN_STANDBY, false);
+  pinMode(PIN_CAN_BOOSTEN, OUTPUT);
+  digitalWrite(PIN_CAN_BOOSTEN, true);
 
-  if(!CAN.begin(500000)){
+  if (!CAN.begin(500000)) {
     Serial.println("Starting CAN failed!");
-    while(1);
+    while (1)
+      ;
   }
 }
 
@@ -85,48 +86,48 @@ void loop() {
 
   blink();
 
-  digitalWrite(CS,LOW);
+  digitalWrite(CS, LOW);
   SPI.transfer(0xA9);
   x_acc = SPI.transfer(0x00);
-  digitalWrite(CS,HIGH);
+  digitalWrite(CS, HIGH);
 
-  digitalWrite(CS,LOW);
+  digitalWrite(CS, LOW);
   SPI.transfer(0xAB);
   y_acc = SPI.transfer(0x00);
-  digitalWrite(CS,HIGH);
+  digitalWrite(CS, HIGH);
 
-  digitalWrite(CS,LOW);
+  digitalWrite(CS, LOW);
   SPI.transfer(0xAD);
   z_acc = SPI.transfer(0x00);
-  digitalWrite(CS,HIGH);
+  digitalWrite(CS, HIGH);
 
-  x_avgs[datacount % rollingAverage]=x_acc;
-  y_avgs[datacount % rollingAverage]=y_acc;
-  z_avgs[datacount % rollingAverage]=z_acc;
+  x_avgs[datacount % rollingAverage] = x_acc;
+  y_avgs[datacount % rollingAverage] = y_acc;
+  z_avgs[datacount % rollingAverage] = z_acc;
 
-  
-  
-  if(datacount>=(rollingAverage-1)){
+
+
+  if (datacount >= (rollingAverage - 1)) {
 
     x_tot = 0;
     y_tot = 0;
     z_tot = 0;
 
-    for (int i=0; i<rollingAverage; i++){
-      x_tot+=x_avgs[i];
-      y_tot+=y_avgs[i];
-      z_tot+=z_avgs[i];
+    for (int i = 0; i < rollingAverage; i++) {
+      x_tot += x_avgs[i];
+      y_tot += y_avgs[i];
+      z_tot += z_avgs[i];
     }
 
-    
 
-    x_send = (byte)(x_tot/rollingAverage);
-    y_send = (byte)(y_tot/rollingAverage);
-    z_send = (byte)(z_tot/rollingAverage);
 
-    
-    unsigned long averageTime=0;
-    if(millis()-averageTime>10){
+    x_send = (byte)(x_tot / rollingAverage);
+    y_send = (byte)(y_tot / rollingAverage);
+    z_send = (byte)(z_tot / rollingAverage);
+
+
+    unsigned long averageTime = 0;
+    if (millis() - averageTime > 10) {
       averageTime = millis();
 
       CAN.beginPacket(0x1);
@@ -137,8 +138,8 @@ void loop() {
     }
   }
   datacount += 1;
-  int x = (signed char) x_send;
-  int y = (signed char) y_send;
-  int z = (signed char) z_send;
-  Serial.printf("X:%d\tY:%d\tZ:%d\n",x,y,z);
+  int x = (signed char)x_send;
+  int y = (signed char)y_send;
+  int z = (signed char)z_send;
+  Serial.printf("X:%d\tY:%d\tZ:%d\n", x, y, z);
 }
--- sensorboard/sensorboard.ino
+++ sensorboard/sensorboard.ino
@@ -1,35 +1,45 @@
 #include <CAN.h>
 #include <SPI.h>
 
+//#include <GoblinMode.h>
+
 #define BAUD_RATE 1000000
+#define ROLLING_AVG 64
 
-byte x_accel;
-byte y_accel;
-byte z_accel;
+byte x_acc;
+byte y_acc;
+byte z_acc;
 
-int datacount= 0;
+byte x_send;
+byte y_send;
+byte z_send;
+
+int datacount = 0;
+
+byte x_avgs[ROLLING_AVG];
+byte y_avgs[ROLLING_AVG];
+byte z_avgs[ROLLING_AVG];
+
+long x_tot;
+long y_tot;
+long z_tot;
 
 const int ACCEL_PIN = 5;
 const int LED = 13;
 
 int ledState = LOW;
 unsigned long currentMillis = millis();
-unsigned long previousMillis = 0; 
+unsigned long previousMillis = 0;
 const long interval = 1000;
 
 // Method for status indicator
-void blink()
-{
+void blink() {
   currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) 
-  {
+  if (currentMillis - previousMillis >= interval) {
     previousMillis = currentMillis;
-    if (ledState == LOW) 
-    {
+    if (ledState == LOW) {
       ledState = HIGH;
-    } 
-    else 
-    {
+    } else {
       ledState = LOW;
     }
     digitalWrite(LED, ledState);
@@ -37,8 +47,7 @@ void blink()
 }
 
 // Configured for + or - 2g
-void setup() 
-{
+void setup() {
   pinMode(LED, OUTPUT);
 
   Serial.begin(11520);
@@ -48,10 +57,10 @@ void setup()
   SPI.begin();
   SPI.beginTransaction(SPISettings(10000000, MSBFIRST, SPI_MODE3));
 
-  digitalWrite(ACCEL_PIN,LOW);
+  digitalWrite(ACCEL_PIN, LOW);
   SPI.transfer(0x20);
   SPI.transfer(0x3F);
-  digitalWrite(ACCEL_PIN,HIGH);
+  digitalWrite(ACCEL_PIN, HIGH);
 
 
   pinMode(PIN_CAN_STANDBY, OUTPUT);
@@ -59,44 +68,65 @@ void setup()
   pinMode(PIN_CAN_BOOSTEN, OUTPUT);
   digitalWrite(PIN_CAN_BOOSTEN, true);
 
-  if(!CAN.begin(BAUD_RATE))
-  {
+  if (!CAN.begin(BAUD_RATE)) {
     Serial.println("Starting CAN failed!");
-    while(1);
+    while (1)
+      ;
   }
 }
 
-void loop() 
-{
+void loop() {
   blink();
 
-  digitalWrite(ACCEL_PIN,LOW);
+  digitalWrite(ACCEL_PIN, LOW);
   SPI.transfer(0xA9);
-  x_accel= SPI.transfer(0x00);
-  digitalWrite(ACCEL_PIN,HIGH);
+  x_acc = SPI.transfer(0x00);
+  digitalWrite(ACCEL_PIN, HIGH);
 
-  digitalWrite(ACCEL_PIN,LOW);
+  digitalWrite(ACCEL_PIN, LOW);
   SPI.transfer(0xAB);
-  y_accel= SPI.transfer(0x00);
-  digitalWrite(ACCEL_PIN,HIGH);
+  y_acc = SPI.transfer(0x00);
+  digitalWrite(ACCEL_PIN, HIGH);
 
-  digitalWrite(ACCEL_PIN,LOW);
+  digitalWrite(ACCEL_PIN, LOW);
   SPI.transfer(0xAD);
-  z_accel= SPI.transfer(0x00);
-  digitalWrite(ACCEL_PIN,HIGH);
+  z_acc = SPI.transfer(0x00);
+  digitalWrite(ACCEL_PIN, HIGH);
+
+  // Averaging
+  x_avgs[datacount % ROLLING_AVG] = x_acc;
+  y_avgs[datacount % ROLLING_AVG] = y_acc;
+  z_avgs[datacount % ROLLING_AVG] = z_acc;
+
+  x_tot = 0;
+  y_tot = 0;
+  z_tot = 0;
+
+  for (int i = 0; i < ROLLING_AVG; i++) {
+    x_tot += x_avgs[i];
+    y_tot += y_avgs[i];
+    z_tot += z_avgs[i];
+  }
+
+  x_send = (byte)(x_tot / ROLLING_AVG);
+  y_send = (byte)(y_tot / ROLLING_AVG);
+  z_send = (byte)(z_tot / ROLLING_AVG);
 
   // Delta time
-  int averageTime=0;
-  if(millis()-averageTime>10)
-  {
-    averageTime=millis();
+  unsigned long averageTime = 0;
+  if (millis() - averageTime > 10) {
+    averageTime = millis();
     CAN.beginPacket(0x1);
-    Serial.printf("X:%d\tY:%d\tZ:%d\n", x_accel, y_accel, z_accel);
-    CAN.write(x_accel);
-    CAN.write(y_accel);
-    CAN.write(z_accel);
+    CAN.write(x_send);
+    CAN.write(y_send);
+    CAN.write(z_send);
     CAN.endPacket();
   }
 
+  datacount += 1;
+
+  int x = (signed char)x_send;
+  int y = (signed char)y_send;
+  int z = (signed char)z_send;
+  Serial.printf("X:%d\tY:%d\tZ:%d\n", x, y, z);
 }
-  
