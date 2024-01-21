4fe076c953b822a101861de5783731098695cc43
andrialuo <143226697+andrialuo@users.noreply.github.com>
2024-01-21 16:28:52 -0600

    Add files via upload

---
+++ 2024/PDM/PDM.c
@@ -0,0 +1,26 @@
+
+/*
+* Gets current measurement from sensor at pin as 16-bit unsigned integer
+* @param pin Pin identifier for current sensor
+*/
+void currSense(int pin);
+
+/*
+* Sets relay state using SPI communication
+* @param fd File descriptor associated with SPI device
+* @param relayState Desired relay state
+*/
+void setRelayState(int fd, uint8_t relayState);
+
+/*
+* Initializes SPI device with SPI mode, bits per word, SPI speed
+*
+* @return 0 if successful
+*/
+int relay();
+
+/*
+* Continuously reads measurements from temperature sensor and converts to integer values
+*/
+void temperature();
+
